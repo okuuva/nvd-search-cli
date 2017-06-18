422fe0e91b6ac4c3af3b0861e9f0c33bbee65277
Oula Kuuva <oula.kuuva@synopsys.com>
2017-06-18 18:14:18 +0300

    Add CLI template

---
+++ nvd-cli.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/docopt/docopt-go"
+)
+
+func main() {
+	usage := `Usage: nvd-search [-c CVE | -k KEY] [-v VENDOR] [-p PRODUCT] [-n NVD]
+
+Options:
+ -h --help                      show this
+ -c CVE --cve CVE               CVE-ID of the vulnerability [default: ]
+ -k KEY --key KEY               keyword search [default: ]
+ -v VENDOR --vendor VENDOR      CPE vendor name [default: ]
+ -p PRODUCT --product PRODUCT   CPE product name [default: ]
+ -n NVD --nvd NVD               Location of the local NVD [default: ./nvd]
+`
+	args, _ := docopt.Parse(usage, nil, true, "nvd-search 0.1", false)
+	fmt.Println(args)
+	arguments := parseArgs(args)
+	fmt.Println(arguments)
+}
